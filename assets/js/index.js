fafd966015e4fb2de37b9082fec2ae0ee84daee8
pedrohenrique57 <pedro.henrique.contato369@gmail.com>
2024-02-05 19:46:21 -0300

    Criação das funções de criptografia. Criação dos métodos para criptografar e descriptografar. Criação do método para alterar o HTML com o resultados da cripto/descriptografia. Criação de um observador para remover ou adicionar a imagem em saidaTexto de acordo com o media query. Alterações no CSS para os métodos que alteram o html e correção de bug por herança na responsividade.
    Signed-off-by: pedrohenrique57 <pedro.henrique.contato369@gmail.com>

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,62 @@
+function botaoCriptografar() {
+  let texto = document.getElementById('caixaDeTexto').value;
+  let deslocamento = 1;
+  let resultado = '';
+
+  for (let i = 0; i < texto.length; i++) {
+    let caractere = texto[i];
+
+    if (/[a-z]/.test(caractere)) {
+      let codigoASCII = caractere.charCodeAt(0);
+
+      codigo = ((codigoASCII - 97 + deslocamento) % 26) + 97;
+
+      resultado += String.fromCharCode(codigo);
+    } else {
+      resultado += ' ';
+    }
+  }
+  saidaTextoHTML(resultado);
+}
+
+function botaoDescriptografar() {
+  let texto = document.getElementById('caixaDeTexto').value;
+  let deslocamento = 1;
+  let resultado = '';
+
+  for (let i = 0; i < texto.length; i++) {
+    let caractere = texto[i];
+
+    if (/[a-z]/.test(caractere)) {
+      let codigoASCII = caractere.charCodeAt(0);
+
+      codigo = ((codigoASCII - 97 - deslocamento + 26) % 26) + 97;
+
+      resultado += String.fromCharCode(codigo);
+    } else {
+      resultado += ' ';
+    }
+  }
+  saidaTextoHTML(resultado);
+}
+
+function saidaTextoHTML(resultado) {
+  const saidaTexto = document.getElementById('saidaTexto');
+
+  const fraseCriptografadaHtml = `
+  <span id="resultadoCripto_Descripto">${resultado}</span>
+  <input id="botaoCopiar" onclick="botaoCopiar()" type="button" value="Copiar">`;
+
+  saidaTexto.innerHTML = fraseCriptografadaHtml;
+}
+
+function botaoCopiar() {
+  const saidaTexto = document.getElementById('resultadoCripto_Descripto').innerText;
+
+  navigator.clipboard.writeText(saidaTexto);
+}
+
+// Remove o placeholder do input de texto quando ele está com o foco
 document.addEventListener('DOMContentLoaded', function () {
   var caixaDeTexto = document.getElementById('caixaDeTexto');
 
@@ -13,3 +72,32 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 });
+
+// Remove a imagem do saidaTexto se o media query fica to tamanho mobile (992px para menor)
+//Ainda não entendi a logica do da linha 80 E 99 (parece estar invertido) MAS DEU CERTO ENTAO VAMOS
+function removeImagem() {
+  const saidaTexto = document.getElementById('saidaTexto');
+  if (mediaQuery.matches) {
+    const saidaTextoSemImagem = `
+    <div class="semTextoInformativo">
+    <h2>Nenhuma mensagem encontrada</h2>
+    <span>Digite um texto que voce deseja Criptografar ou Descriptografar</span>
+  </div>`;
+
+    saidaTexto.innerHTML = saidaTextoSemImagem;
+  } else {
+    const saidaTextoSemImagem = `
+    <img src="./assets/img/MulherComLupa.svg" alt="Imagem de uma mulher observando um diamante com uma lupa." />
+    <div class="semTextoInformativo">
+      <h2>Nenhuma mensagem encontrada</h2>
+      <span>Digite um texto que voce deseja Criptografar ou Descriptografar</span>
+    </div>`;
+
+    saidaTexto.innerHTML = saidaTextoSemImagem;
+  }
+}
+const mediaQuery = window.matchMedia('(max-width: 992px)');
+
+mediaQuery.addEventListener('change', (e) => removeImagem(e.media));
+
+removeImagem(mediaQuery);
